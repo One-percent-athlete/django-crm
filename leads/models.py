f72089a8dfd61d631f22aff7925ea5324257af93
One-percent-athlete <one.percent.athlete@gmail.com>
2024-09-07 20:08:49 +0900

    Create home page view and url

--- leads/models.py
+++ leads/models.py
@@ -7,8 +7,14 @@ class User(AbstractUser):
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    age = models.IntegerField(default=0)
+    age = models.IntegerField(default=18)
     agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.user.email}"

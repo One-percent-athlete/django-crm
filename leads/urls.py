49ca65470e255a99d0309670728803f9105b3db3
One-percent-athlete <one.percent.athlete@gmail.com>
2024-09-09 03:05:18 +0900

    Add update lead view and page

--- leads/urls.py
+++ leads/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.home_page, name='home'),
     path('leads_all/', views.lead_list, name='lead_list'),
     path('leads_create/', views.lead_create, name='lead_create'),
-    path('leads/<int:pk>/', views.lead_detail, name='lead_detail'),
+    path('<int:pk>/', views.lead_detail, name='lead_detail'),
+    path('<int:pk>/update', views.lead_update, name='lead_update'),
 ]

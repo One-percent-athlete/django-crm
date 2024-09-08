5a273d0b34739bf7fbfd9857d5f44a4af6152928
One-percent-athlete <one.percent.athlete@gmail.com>
2024-09-09 03:12:28 +0900

    Add delete view

--- leads/urls.py
+++ leads/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('leads_create/', views.lead_create, name='lead_create'),
     path('<int:pk>/', views.lead_detail, name='lead_detail'),
     path('<int:pk>/update', views.lead_update, name='lead_update'),
+    path('<int:pk>/delete', views.lead_delete, name='lead_delete'),
 ]

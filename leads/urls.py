18c23612b951bbffa1d020b6a26135b2c4474e1c
One-percent-athlete <one.percent.athlete@gmail.com>
2024-09-09 00:24:56 +0900

    Modify urls and home page

--- leads/urls.py
+++ leads/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path
-import views
+from leads import views
+
+app_name = 'leads'
 
 urlpatterns = [
     path('', views.home_page, name='home'),
-  
+    path('leads_all/', views.lead_list, name='lead_list'),
 ]
--- leads/views.py
+++ leads/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render
 from .models import Lead
 
 def home_page(request):
+    return render(request, 'home_page.html')
+
+
+def lead_list(request):
     leads = Lead.objects.all()
 
     context = {"leads": leads}
 
-    return render(request, 'home_page.html', context)
+    return render(request, 'leads/lead_list.html', context)

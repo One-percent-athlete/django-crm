040eece2f7a703436ef811dc36a280eb02ab79bd
One-percent-athlete <one.percent.athlete@gmail.com>
2024-09-09 00:35:49 +0900

    Add detail page and view

--- leads/urls.py
+++ leads/urls.py
@@ -7,4 +7,5 @@ app_name = 'leads'
 urlpatterns = [
     path('', views.home_page, name='home'),
     path('leads_all/', views.lead_list, name='lead_list'),
+    path('leads/<str:pk>/', views.lead_detail, name='lead_detail'),
 ]

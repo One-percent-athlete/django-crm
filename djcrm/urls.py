3b7113cd3d472927427ed992edc361c8da800bf2
One-percent-athlete <one.percent.athlete@gmail.com>
2024-09-13 20:42:17 +0900

    Add login view and page

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
+
 from django.urls import path, include
 
 # from leads.views import home_page
@@ -10,7 +12,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
     path('leads/', include('leads.urls', namespace='leads')),
-    
+    path('login/', LoginView.as_view(), name='login'),
 ]
 
 if settings.DEBUG:

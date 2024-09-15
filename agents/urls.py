1486b9883dd607bd1a8386c38c10eb87ee2423b3
One-percent-athlete <one.percent.athlete@gmail.com>
2024-09-16 02:10:23 +0900

    Add agent update view and page

--- agents/urls.py
+++ agents/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import AgentListView, AgentCreateView, AgentDetailView
+from .views import AgentListView, AgentCreateView, AgentDetailView, AgentUpdateView
 
 app_name = 'agents'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', AgentListView.as_view(), name='agent_list'),
     path('agent_create/', AgentCreateView.as_view(), name='agent_create'),
     path('<int:pk>/', AgentDetailView.as_view(), name='agent_detail'),
+    path('<int:pk>/update', AgentUpdateView.as_view(), name='agent_update'),
 ]
--- agents/views.py
+++ agents/views.py
@@ -25,11 +25,19 @@ class AgentCreateView(LoginRequiredMixin, generic.CreateView):
         agent.organization = self.request.user.userprofile
         agent.save()
         return super(AgentCreateView, self).form_valid(form)
-    
-    
-    
+
 
 class AgentDetailView(LoginRequiredMixin, generic.DetailView):
     template_name = 'agents/agent_detail.html'
     queryset = Agent.objects.all()
     context_object_name = 'agent'
+
+
+class AgentUpdateView(LoginRequiredMixin, generic.UpdateView):
+    template_name = 'agents/agent_update.html'
+    queryset = Agent.objects.all()
+    form_class = AgentModelForm
+
+    def get_success_url(self):
+        return reverse('agents:agent_list')
+    

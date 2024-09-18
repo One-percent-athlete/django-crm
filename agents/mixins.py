b15bb83d18bd9f9e26a15339c562292c7e0e30e1
One-percent-athlete <one.percent.athlete@gmail.com>
2024-09-18 16:12:49 +0900

    Update redirect url for agent user

--- agents/mixins.py
+++ agents/mixins.py
@@ -5,5 +5,5 @@ class OrganizerLoginRequiredMixin(AccessMixin):
     """Verify that the current user is authenticated and is organizer."""
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated or not request.user.is_organizer:
-            return redirect('login')
+            return redirect('leads:lead_list')
         return super().dispatch(request, *args, **kwargs)

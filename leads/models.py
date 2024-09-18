fcbd4ff6c764d0b6953ff1bf95e96a6b9925c590
One-percent-athlete <one.percent.athlete@gmail.com>
2024-09-18 15:36:30 +0900

    Modify user model

--- leads/models.py
+++ leads/models.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import AbstractUser
 
     
 class User(AbstractUser):
-    pass
+    is_organizar = models.BooleanField(default=True)
+    is_agent = models.BooleanField(default=False)
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)

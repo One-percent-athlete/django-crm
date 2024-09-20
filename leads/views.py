5464769e8d98fb45ed14423eeb5f868369463dea
One-percent-athlete <one.percent.athlete@gmail.com>
2024-09-21 01:04:33 +0900

    Modify category page, add unassigned lead count section

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.core.mail import send_mail
 from django.shortcuts import render, redirect, reverse
 from django.views import generic
@@ -209,6 +210,21 @@ class AssignAgentView(OrganizerLoginRequiredMixin, generic.FormView):
 class CategoryListView(LoginRequiredMixin, generic.ListView):
     template_name = 'leads/category_list.html'
     context_object_name = 'category_list'
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        user = self.request.user
+
+        if user.is_organizer:
+            queryset = Lead.objects.filter(organization=user.userprofile)
+        else:
+            queryset = Lead.objects.filter(organization=user.agent.organization)
+
+        context.update({
+            'unassigned_lead_count': queryset.filter(category__isnull=True).count()
+        })
+
+        return context
 
     def get_queryset(self):
         user = self.request.user
